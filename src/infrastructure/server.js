e98c7b529268f143d31b22b4b5694728ebcab35a
Mateo Ruiz <teiito.ruiz13@gmail.com>
2024-01-09 11:21:46 -0500

    Add database configuration and update user routes

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+	Pool
+} = require('pg');
+
+//configuracion de la base de datos
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'hexagonal-nodejs-ejemplo',
+	password: '14122004',
+	port: 5432,
+});
 
 function configureServer(UserHttpAdapter) {
 	const app = express();
@@ -10,21 +22,37 @@ function configureServer(UserHttpAdapter) {
 	app.post("/users", (req, res) => UserHttpAdapter.createUser(req, res));
 
 	// Nueva ruta get para obtener todos los usuarios
-	app.get("/users", (req, res) => {
-		const users = UserHttpAdapter.getAllUsers(res);
-		res.json(users);
+	app.get("/users", async (req, res) => {
+		try {
+			const result = await pool.query('SELECT * FROM users');
+			const users = result.rows;
+			res.json(users);
+		} catch (error) {
+			console.error("Error al obtener los usuarios", error)
+			res.status(500).json({
+				message: "Error al obtener los usuarios"
+			});
+		}
 	});
 
 	// Nueva ruta get para obtener un usuario por id
-	app.get("/users/:id", (req, res) => {
+	app.get("/users/:id", async (req, res) => {
 		const userId = req.params.id;
-		const user = UserHttpAdapter.getUserById(userId);
+		try {
+			const result = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
+			const user = result.rows[0];
+			if (user) {
+				res.json(user);
+			} else {
+				res.status(404).json({
+					message: "User not found"
+				});
+			}
 
-		if (user) { 
-			res.json(user);
-		} else {
-			res.status(404).json({
-				message: "User not found"
+		} catch (error) {
+			console.error("Error al obtener el usuario", error)
+			res.status(500).json({
+				message: "Error al obtener el usuario"
 			});
 		}
 	});
